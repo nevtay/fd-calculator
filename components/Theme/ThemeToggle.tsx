502ff9342d0f15b6d6ef0eb61d62e9112fa0dcb6
Nev <nev@Nevs-MacBook-Pro.local>
2026-07-13 01:27:32 +0800

    feat: update configs and packages to handle dark mode

---
+++ components/Theme/ThemeToggle.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Prevent hydration mismatch by waiting until mounted on client
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  console.log("theme", theme);
+
+  if (!mounted) {
+    return <div className="w-9 h-9 bg-gray-200 rounded-md dynamic-skeleton" />;
+  }
+
+  return (
+    <button
+      aria-label="Toggle Theme"
+      className="p-2 border rounded-md cursor-pointer bg-neutral-100 dark:bg-neutral-800 dark:text-amber-300 text-purple-500 w-50 ml-auto"
+      onClick={(e) => {
+        e.preventDefault();
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+    >
+      Current Theme: <span className="font-bold capitalize">{theme}</span>
+    </button>
+  );
+}
