647a6f2cc5ee40fdca6e12413fc153fc569b1079
Nevin Tay <nevintay@gmail.com>
2026-07-28 16:22:31 +0800

    feat: position ThemeToggle button alongside main header

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import { ThemeToggle } from "@/components/Theme/ThemeToggle";
 
 export default function Home() {
   return (
-    <div>
-      <ThemeToggle />
-      <div className="min-h-screen px-12 py-12">
-        <h1 className="text-(--color-indigo) font-semibold text-5xl mb-12">
-          Fixed Deposit Calculator
-        </h1>
+    <div className="w-full">
+      <div className="min-h-screen w-10/12 m-auto mt-12 flex flex-col">
+        <div className="mb-12 inline-flex flex-row justify-between items-center">
+          <h1 className="text-(--color-indigo) font-semibold text-5xl w-fit">
+            Fixed Deposit Calculator
+          </h1>
+          <ThemeToggle />
+        </div>
         <Calculator />
       </div>
     </div>
