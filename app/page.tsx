229e9649d0bf373c91fde229a9e062fd59d22725
Nevin Tay <nevintay@gmail.com>
2026-07-29 14:59:20 +0800

    chore/style: format class names and apply flex column layout to header and ThemeToggle elements at md breakpoint

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { ThemeToggle } from "@/components/Theme/ThemeToggle";
 export default function Home() {
   return (
     <div className="w-full">
-      <div className="min-h-screen w-10/12 m-auto mt-12 flex flex-col">
-        <div className="mb-12 inline-flex flex-row justify-between items-center">
-          <h1 className="text-(--color-indigo) font-semibold text-5xl w-fit">
+      <div className="m-auto mt-12 flex min-h-screen w-10/12 flex-col">
+        <div className="mb-12 inline-flex flex-row items-center justify-between max-md:flex max-md:flex-col max-md:gap-4">
+          <h1 className="w-fit text-5xl font-semibold text-(--color-indigo) max-md:text-center">
             Fixed Deposit Calculator
           </h1>
           <ThemeToggle />
--- components/Theme/ThemeToggle.tsx
+++ components/Theme/ThemeToggle.tsx
@@ -13,19 +13,20 @@ export function ThemeToggle() {
   }, []);
 
   if (!mounted) {
-    return <div className="w-9 h-9 bg-gray-200 rounded-md dynamic-skeleton" />;
+    return <div className="dynamic-skeleton h-9 w-9 rounded-md bg-gray-200" />;
   }
 
   return (
     <button
       aria-label="Toggle Theme"
-      className="p-2 border rounded-md cursor-pointer bg-neutral-100 dark:bg-neutral-800 dark:text-amber-300 text-purple-500 w-fit h-fit"
+      className="h-fit w-fit cursor-pointer rounded-md border bg-neutral-100 p-2 text-purple-500 dark:bg-neutral-800 dark:text-amber-300"
       onClick={(e) => {
         e.preventDefault();
         setTheme(resolvedTheme === "dark" ? "light" : "dark");
       }}
     >
-      Current Theme: <span className="font-bold capitalize">{resolvedTheme}</span>
+      Current Theme:{" "}
+      <span className="font-bold capitalize">{resolvedTheme}</span>
     </button>
   );
 }
