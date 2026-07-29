46916292852299a9071ae50a5da9b68aa22685c8
Nevin Tay <nevintay@gmail.com>
2026-07-29 17:02:15 +0800

    feat: update text-shadow values

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="m-auto mt-8 flex min-h-screen w-10/12 flex-col">
         <div className="mb-12 inline-flex flex-row items-center justify-between max-md:flex max-md:flex-col max-md:gap-4">
           <div className="w-12.5/12 relative left-[-20vw] h-auto rounded-2xl bg-indigo-500 py-6 shadow-[-0px_-0px_8px_var(--skeu-shadow),4px_4px_8px_var(--skeu-shadow)] text-shadow-[-2px_-2px_4px_var(--skeu-highlight),4px`1_4px_4px_var(--skeu-shadow)] max-md:left-[-15vw]">
-            <h1 className="relative left-[20vw] w-fit rounded-2xl bg-indigo-200 px-5 py-10 text-5xl font-semibold text-(--color-indigo) shadow-[-0px_-0px_8px_var(--skeu-shadow),4px_4px_8px_var(--skeu-shadow)] text-shadow-[-2px_-2px_4px_var(--skeu-highlight),4px`1_4px_4px_var(--skeu-shadow)] max-md:left-[15vw] max-md:w-12/12 max-md:text-center">
+            <h1 className="relative left-[20vw] w-fit rounded-2xl bg-indigo-200 px-5 py-10 text-5xl font-semibold text-(--color-indigo) shadow-[-0px_-0px_8px_var(--skeu-shadow),4px_4px_8px_var(--skeu-shadow)] text-shadow-[0.65px_0.60px_0.25px_var(--skeu-highlight-weak),1px_1px_2px_var(--skeu-shadow)] max-md:left-[15vw] max-md:w-12/12 max-md:text-center">
               Fixed Deposit Calculator
             </h1>
           </div>
--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -238,7 +238,7 @@ const Calculator = () => {
         </div>
         <div className="m-auto mb-8 flex w-12/12">
           <button
-            className="bg-input-container m-auto h-fit w-auto cursor-pointer rounded-2xl px-6 py-2 text-(--color-body-text) text-shadow-[-1px_-1px_1px_var(--skeu-highlight-weak),1px_1px_2px_var(--skeu-shadow)] hover:scale-90 md:ml-auto"
+            className="bg-input-container m-auto h-fit w-auto cursor-pointer rounded-2xl px-6 py-2 text-(--color-body-text) shadow-[-0px_-0px_4px_var(--skeu-shadow),4px_4px_4px_var(--skeu-shadow)] text-shadow-[.6px_.60px_0.25px_var(--skeu-highlight-weak),1px_1px_2px_var(--skeu-shadow)] hover:scale-90 md:ml-auto"
             type="reset"
             name="Reset"
             onClick={handleReset}
