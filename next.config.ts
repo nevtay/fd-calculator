c1a7c8ed41f2f92a7497546c6467d4329fa2e73e
Nevin Tay <nevintay@gmail.com>
2026-07-29 14:03:00 +0800

    feat: update next.config.ts to handle github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/fd-calculator" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
