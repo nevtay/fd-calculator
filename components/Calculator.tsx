10cf54de6a693483a3b7f90aa007b29c7aeac659
Nevin Tay <nevintay@gmail.com>
2026-08-02 14:26:11 +0800

    feat: remove unused ThemeToggle import

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useEffect, useState } from "react";
-import { ThemeToggle } from "./Theme/ThemeToggle";
 import {
   growthSeries,
   interestEarned as calculateInterestEarned,
